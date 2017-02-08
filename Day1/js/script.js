9fb5e652ae8e6c1d82d3558f8990a397bfebc6b1
samatar axmed <Samatar_267@hotmail.com>
2017-02-08 18:43:16 +0000

    Added corresponsing keydown sound effects

--- Day1/js/script.js
+++ Day1/js/script.js
@@ -6,6 +6,7 @@ function button(){return document.createElement("button")};
 function div(){return document.createElement("div")};
 function p(){return document.createElement("p")};
 var buttonkeys = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
+var keycodes = [65, 83, 68, 70, 71, 72, 74, 75, 76];
 var soundTexts = ["clap", "Hihat", "Kick", "Openhat", "Boom",
 "ride", "snare", "Tom", "Tink"];
 //Background Image
@@ -33,18 +34,35 @@ for (var i = 0; i<9; i++){
 
 //SoundPlay function
 function play(el){
-  var sound = new Audio("sounds/"+el.target.id+".wav");
-  sound.play();
+  for (var j = 0; j<keycodes.length; j++){
+
+      if (el.keyCode===keycodes[j]){
+        var sound = new Audio("sounds/"+soundTexts[j]+".wav");
+        sound.play();
+        var event = document.getElementById(soundTexts[j]);
+        event.style.borderColor="Yellow";
+        event.style.fontSize= "3.1em";
+
+
+        setTimeout(function(){
+            event.style.borderColor="Black";
+            event.style.fontSize = "3em";
+
+
+        }, 300)
+      }
+
+
+  }
+
 }
 
 
 
-//Add EventListeners
-for (var i = 0; i<soundTexts.length; i++) {
+//Add EventListener
+    window.addEventListener("keydown", play);
 
-    document.getElementById(soundTexts[i]).addEventListener("click", play);
 
-}
 
 
 }
