e889e8ffdeb7f5db15aaeb72d8792e6ba8f1c8a6
samatar axmed <Samatar_267@hotmail.com>
2017-02-08 17:00:47 +0000

    Added Button Keys

--- Day1/js/script.js
+++ Day1/js/script.js
@@ -1,15 +1,28 @@
 window.onload = function(){
 
-function body(){return document.getElementById("body")};
+var body = document.getElementById("body");
 function image(){return document.createElement("img")};
 function button(){return document.createElement("button")};
-
+function div(){return document.createElement("div")};
+var buttonkeys = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
 
 //Background Image
 var backgroundImage = image();
 backgroundImage.src = "images/background-18.jpg";
 backgroundImage.alt = "Psychedelic Background";
-body().appendChild(backgroundImage);
+body.appendChild(backgroundImage);
+
+//Append div
+var div = div();
+body.appendChild(div);
+
+//Create Drumkit Buttons
+
+for (var i = 0; i<9; i++){
+  var buttons = button();
+  buttons.innerText=buttonkeys[i];
+  div.appendChild(buttons);
+}
 
 
 
