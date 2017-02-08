ad4ec1d1014fcbde97fbb12498c7fbc5b0c83c4f
samatar axmed <Samatar_267@hotmail.com>
2017-02-08 18:03:29 +0000

    Add Sound Text

--- Day1/js/script.js
+++ Day1/js/script.js
@@ -4,8 +4,10 @@ var body = document.getElementById("body");
 function image(){return document.createElement("img")};
 function button(){return document.createElement("button")};
 function div(){return document.createElement("div")};
+function p(){return document.createElement("p")};
 var buttonkeys = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
-
+var soundTexts = ["clap", "Hihat", "Kick", "Openhat", "Boom",
+"ride", "snare", "Tom", "Tink"];
 //Background Image
 var backgroundImage = image();
 backgroundImage.src = "images/background-18.jpg";
@@ -20,7 +22,11 @@ body.appendChild(div);
 
 for (var i = 0; i<9; i++){
   var buttons = button();
+  var paragraph = p();
+  paragraph.innerText = soundTexts[i];
+  console.log(paragraph);
   buttons.innerText=buttonkeys[i];
+  buttons.appendChild(paragraph);
   div.appendChild(buttons);
 }
 
